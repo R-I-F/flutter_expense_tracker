5c3c32fc882a82bafadd0f6e919ac270e0012fa8
Ibrahim El-Refaee <ibrahimrefaeei@gmail.com>
2024-08-02 17:19:28 +0300

    feature: delete expense item

--- lib/providers/expense_provider.dart
+++ lib/providers/expense_provider.dart
@@ -1,9 +1,10 @@
 import 'package:ex2_expense_tracker/models/expenseModel.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path/path.dart' as path;
 import 'package:sqflite/sqflite.dart' as sql;
 import 'package:sqflite/sqlite_api.dart';
-import 'package:intl/intl.dart'; 
+import 'package:intl/intl.dart';
 
 final formatter = DateFormat.yMd();
 
@@ -27,31 +28,18 @@ class ExpenseProviderNotifier extends StateNotifier<List<ExpenseModel>> {
     if (data.isEmpty) {
       return;
     }
-    print(data);
 
-    print('populating');
     state = data.map((row) {
-
       String categoryName = row['category'] as String;
       String dateHolder = row['date'] as String;
-
-
       final id = row['id'] as String;
-      print(id);
       final name = row['name'] as String;
-      print(name);
       final amount = (row['amount'] as num).toDouble();
-      print(amount);
-      final cat = Category.values.firstWhere((item)=>categoryName == item.name);
-      print(cat);
+      final cat =
+          Category.values.firstWhere((item) => categoryName == item.name);
       final date = formatter.parse(dateHolder);
-      print(date);
       return ExpenseModel(
-          insertedId: id,
-          title: name,
-          cost: amount,
-          category: cat,
-          date: date);
+          insertedId: id, title: name, cost: amount, category: cat, date: date);
     }).toList();
   }
 
@@ -79,12 +67,42 @@ class ExpenseProviderNotifier extends StateNotifier<List<ExpenseModel>> {
     ];
     return;
   }
-void deleteDatabase() async {
-  final db = await _getDatabase();
-  await db.delete('expenses_list');
-}
-}
 
+  void deleteExpense(String id, BuildContext context) async {
+    final expense = state.firstWhere((item) => item.id == id);
+    final expenseIndex =
+        state.indexOf(expense);
+
+    state = state.where((item) => item.id != id).toList();
+    ScaffoldMessenger.of(context).clearSnackBars();
+    final isItemDeleted =  await ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text(
+            'You just Deleted an expense, do you wish to restore it ?'),
+        action: SnackBarAction(label: 'Restore', onPressed: (){
+          List newList = [...state];
+          newList.insert(expenseIndex, expense);
+          state = [...newList];
+        }),
+        duration: const Duration(seconds: 1),
+      ),
+    ).closed.then((result)=>result != SnackBarClosedReason.action);
+    
+    if(!isItemDeleted){ return ; }
+    try{
+      final db = await _getDatabase();
+      await db.delete('expenses_list', where: 'id = ?', whereArgs: [id]);
+    } catch(err){
+      print(err);
+    }
+
+  }
+
+  void deleteDatabase() async {
+    final db = await _getDatabase();
+    await db.delete('expenses_list');
+  }
+}
 
 final expenseProvider =
     StateNotifierProvider<ExpenseProviderNotifier, List<ExpenseModel>>(
--- lib/widgets/expenses_list.dart
+++ lib/widgets/expenses_list.dart
@@ -3,7 +3,6 @@ import 'package:intl/intl.dart';
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import 'package:ex2_expense_tracker/providers/expense_provider.dart';
 
-
 class ExpensesList extends ConsumerStatefulWidget {
   const ExpensesList({super.key});
 
@@ -15,36 +14,70 @@ class _ExpensesListState extends ConsumerState<ExpensesList> {
   final formatter = DateFormat.yMd();
   @override
   Widget build(BuildContext context) {
-  final expenseData = ref.watch(expenseProvider);
-  print(expenseData);
+    final expenseData = ref.watch(expenseProvider);
     return Expanded(
       child: ListView.builder(
-        itemCount: expenseData.length,
-        itemBuilder: (ctx, index) {
-          final dateString = formatter.format(expenseData[index].date);
-          return Card(
-            margin: const EdgeInsets.all(8),
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                    Text(expenseData[index].title, style: Theme.of(context).textTheme.titleMedium,),
-                    Text('\$${expenseData[index].cost.toStringAsFixed(2)}', style: Theme.of(context).textTheme.bodyMedium)
-                  ]),
-                  const Spacer(),
-                  Text(dateString, style: Theme.of(context).textTheme.bodyMedium),
-                  const SizedBox(width: 10,),
-                  Icon(expenseData[index].categoryIcon),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
+          itemCount: expenseData.length,
+          itemBuilder: (ctx, index) {
+            final dateString = formatter.format(expenseData[index].date);
+            return Dismissible(
+                key: ValueKey(expenseData[index].id),
+                background: Container(color: Theme.of(context).colorScheme.error,),
+                onDismissed: (DismissDirection? direction)=>ref.read(expenseProvider.notifier).deleteExpense(expenseData[index].id, ctx),
+                child: Card(
+                  margin: const EdgeInsets.all(8),
+                  child: Container(
+                    padding: const EdgeInsets.all(16),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                expenseData[index].title,
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
+                              Text(
+                                  '\$${expenseData[index].cost.toStringAsFixed(2)}',
+                                  style: Theme.of(context).textTheme.bodyMedium)
+                            ]),
+                        const Spacer(),
+                        Text(dateString,
+                            style: Theme.of(context).textTheme.bodyMedium),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Icon(expenseData[index].categoryIcon),
+                      ],
+                    ),
+                  ),
+                ),);
+          }),
     );
   }
 }
+// (ctx, index) {
+//           final dateString = formatter.format(expenseData[index].date);
+          // return Card(
+          //   margin: const EdgeInsets.all(8),
+          //   child: Container(
+          //     padding: const EdgeInsets.all(16),
+          //     child: Row(
+          //       crossAxisAlignment: CrossAxisAlignment.end,
+          //       children: [
+          //         Column(
+          //           crossAxisAlignment: CrossAxisAlignment.start,
+          //           children: [
+          //           Text(expenseData[index].title, style: Theme.of(context).textTheme.titleMedium,),
+          //           Text('\$${expenseData[index].cost.toStringAsFixed(2)}', style: Theme.of(context).textTheme.bodyMedium)
+          //         ]),
+          //         const Spacer(),
+          //         Text(dateString, style: Theme.of(context).textTheme.bodyMedium),
+          //         const SizedBox(width: 10,),
+          //         Icon(expenseData[index].categoryIcon),
+          //       ],
+          //     ),
+          //   ),
+          // );
+//         },
